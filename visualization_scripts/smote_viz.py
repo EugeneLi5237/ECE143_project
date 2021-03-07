78307fb92e43cc8c5b86fc4d1f99fd4b72d7888b
Sam Cowin <scowin23@gmail.com>
2021-03-06 17:18:39 -0800

    Adding smote visualization script and plots

---
+++ visualization_scripts/smote_viz.py
@@ -0,0 +1,25 @@
+from collections import Counter
+from sklearn.datasets import make_classification
+from imblearn.over_sampling import SMOTE
+from matplotlib import colors
+from matplotlib import pyplot
+from numpy import where
+import numpy as np
+
+X, y = make_classification(n_samples=10000, n_features=2, n_redundant=0,
+        n_clusters_per_class=1, weights=[0.99], flip_y=0, random_state=1)
+oversample = SMOTE()
+X, y = oversample.fit_resample(X, y)
+counter = Counter(y)
+
+for label, _ in counter.items():
+    row_ix = where(y == label)[0]
+    colors = np.array(['gold', 'lemonchiffon'])
+    z = np.zeros(9900, dtype='int')
+    o = np.ones(9900, dtype='int')
+    color = [z, o]
+    index = color[label]
+    pyplot.scatter(X[row_ix, 0], X[row_ix, 1], c=colors[index])
+    ax = pyplot.gca()
+    ax.set_facecolor('xkcd:black')
+pyplot.show()
