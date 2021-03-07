945c9ec4f034ad7e454dae37ce92fe1d24731725
Sam Cowin <scowin23@gmail.com>
2021-03-06 20:21:09 -0800

    Fixed background color

--- visualization_scripts/smote_viz.py
+++ visualization_scripts/smote_viz.py
@@ -8,18 +8,18 @@ import numpy as np
 
 X, y = make_classification(n_samples=10000, n_features=2, n_redundant=0,
         n_clusters_per_class=1, weights=[0.99], flip_y=0, random_state=1)
-oversample = SMOTE()
-X, y = oversample.fit_resample(X, y)
+# oversample = SMOTE()
+# X, y = oversample.fit_resample(X, y)
 counter = Counter(y)
 
 for label, _ in counter.items():
     row_ix = where(y == label)[0]
     colors = np.array(['gold', 'lemonchiffon'])
     z = np.zeros(9900, dtype='int')
-    o = np.ones(9900, dtype='int')
+    o = np.ones(100, dtype='int')
     color = [z, o]
     index = color[label]
     pyplot.scatter(X[row_ix, 0], X[row_ix, 1], c=colors[index])
     ax = pyplot.gca()
-    ax.set_facecolor('xkcd:black')
+    ax.set_facecolor((0.26, 0.26, 0.26))
 pyplot.show()
