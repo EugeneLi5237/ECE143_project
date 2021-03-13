103bb9215718898c07071b647cee8f7f7b21326d
Sam Cowin <scowin23@gmail.com>
2021-03-13 14:20:34 -0600

    Adding docstrings and asserts

--- visualization_scripts/correlation.py
+++ visualization_scripts/correlation.py
@@ -1,14 +1,23 @@
-# Import required library
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Import the CSV file into Python
-A_data = pd.read_csv("c_train.csv")
-A_data = A_data.dropna()
-print(A_data.shape)
 
-# Run correlation on the data and plot heatmap
-heatmap = sns.heatmap(A_data.corr(), cmap="RdYlGn", cbar_kws = dict(use_gridspec=False, location="top", label='Correlation'), xticklabels=2, annot=False, linewidths=.5)
-plt.xticks(rotation=0) 
-plt.show()
+def correlation_plot(fname):
+    """
+    Method to plot the correlation data from a dataset in a .csv file
+    """
+
+    assert isinstance(fname, str)
+    assert len(fname) > 0
+    assert fname[-4:] == ".csv"
+
+    # Import the CSV file into Python
+    A_data = pd.read_csv(fname)
+    A_data = A_data.dropna()
+
+    # Run correlation on the data and plot heatmap
+    heatmap = sns.heatmap(A_data.corr(), cmap="RdYlGn", cbar_kws=dict(
+        use_gridspec=False, location="top", label='Correlation'), xticklabels=2, annot=False, linewidths=.5)
+    plt.xticks(rotation=0)
+    plt.show()
--- visualization_scripts/smote_viz.py
+++ visualization_scripts/smote_viz.py
@@ -1,37 +1,37 @@
 from collections import Counter
 from sklearn.datasets import make_classification
 from imblearn.over_sampling import SMOTE
-from matplotlib import colors
 from matplotlib import pyplot
 from numpy import where
 import numpy as np
 
-def smote_visualization(n=10000, nz=9900, no=100, smote_bool=False):
-        """
-        Method to visualize dataset modifications made by SMOTE algorithm
-        """
-        
-        assert isinstance(n, int)
-        assert isinstance(nz, int)
-        assert isinstance(no, int)
-        assert n > nz >= no > 0
-        assert isinstance(smote_bool, bool)
-        
-        X, y = make_classification(n_samples=n, n_features=2, n_redundant=0,
-                n_clusters_per_class=1, weights=[0.99], flip_y=0, random_state=1)
-        if smote_bool:
-                oversample = SMOTE()
-                X, y = oversample.fit_resample(X, y)
-        counter = Counter(y)
 
-        for label, _ in counter.items():
-            row_ix = where(y == label)[0]
-            colors = np.array(['gold', 'lemonchiffon'])
-            z = np.zeros(nz, dtype='int')
-            o = np.ones(no, dtype='int')
-            color = [z, o]
-            index = color[label]
-            pyplot.scatter(X[row_ix, 0], X[row_ix, 1], c=colors[index])
-            ax = pyplot.gca()
-            ax.set_facecolor((0.26, 0.26, 0.26))
-        pyplot.show()
+def smote_visualization(n=10000, nz=9900, no=100, smote_bool=False):
+    """
+    Method to visualize dataset modifications made by SMOTE algorithm
+    """
+
+    assert isinstance(n, int)
+    assert isinstance(nz, int)
+    assert isinstance(no, int)
+    assert n > nz >= no > 0
+    assert isinstance(smote_bool, bool)
+
+    X, y = make_classification(n_samples=n, n_features=2, n_redundant=0,
+                               n_clusters_per_class=1, weights=[0.99], flip_y=0, random_state=1)
+    if smote_bool:
+        oversample = SMOTE()
+        X, y = oversample.fit_resample(X, y)
+    counter = Counter(y)
+
+    for label, _ in counter.items():
+        row_ix = where(y == label)[0]
+        colors = np.array(['gold', 'lemonchiffon'])
+        z = np.zeros(nz, dtype='int')
+        o = np.ones(no, dtype='int')
+        color = [z, o]
+        index = color[label]
+        pyplot.scatter(X[row_ix, 0], X[row_ix, 1], c=colors[index])
+        ax = pyplot.gca()
+        ax.set_facecolor((0.26, 0.26, 0.26))
+    pyplot.show()
